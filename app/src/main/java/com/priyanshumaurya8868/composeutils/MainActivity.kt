1aa3e83671197124d718e1123492d8fa4befe788
Priyanshu Morya <priyanshumaurya.8868@gmail.com>
2022-03-13 19:22:01 +0530

    animated_progbar via canvas

--- app/src/main/java/com/priyanshumaurya8868/composeutils/MainActivity.kt
+++ app/src/main/java/com/priyanshumaurya8868/composeutils/MainActivity.kt
@@ -3,14 +3,35 @@ package com.priyanshumaurya8868.composeutils
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.priyanshumaurya8868.composeutils.ui.theme.ComposeUtilsTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeUtilsTheme {
-
+                Box(modifier = Modifier.fillMaxSize(),contentAlignment = Alignment.Center) {
+                    AnimatedCircularProgressbar(percentage = 0.6f, number = 200,strokeWidth = 10.dp, radius = 100.dp, animDuration = 2500)
+                }
 
             }
         }
@@ -18,5 +39,56 @@ class MainActivity : ComponentActivity() {
 
 
 }
+@Composable
+fun AnimatedCircularProgressbar(
+    percentage: Float,
+    number: Int,
+    fontSize: TextUnit = 20.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Green,
+    strokeWidth: Dp = 8.dp,
+    animDuration: Int = 1500,
+    animDelay: Int = 0
+) {
 
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+
+    val curPercentage =  animateFloatAsState(
+        targetValue = if (animationPlayed) percentage else 0f,
+        animationSpec = tween(
+            durationMillis = animDuration,
+            delayMillis = animDelay
+        )
+    )
+
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
+
+    Box(
+        modifier = Modifier.size(radius * 2f),
+        contentAlignment = Alignment.Center
+    ) {
+        // TODO: its mandatory to define its size
+        Canvas(modifier = Modifier.size(radius)) {
+            drawArc(
+                color = color,
+                startAngle = -90f,
+                sweepAngle = 360 * curPercentage.value,
+                useCenter = false,
+                style = Stroke(width = strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+
+        }
+        Text(
+            text = (curPercentage.value * number).toInt().toString(),
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+    }
+
+}
 
