ebe8d726ddfa344181c4c67f3805e44213ba01a2
Priyanshu Morya <priyanshumaurya.8868@gmail.com>
2022-03-15 12:03:23 +0530

    motion layout

--- app/src/main/java/com/priyanshumaurya8868/composeutils/MainActivity.kt
+++ app/src/main/java/com/priyanshumaurya8868/composeutils/MainActivity.kt
@@ -3,34 +3,50 @@ package com.priyanshumaurya8868.composeutils
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Slider
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ExperimentalMotionApi
+import androidx.constraintlayout.compose.MotionLayout
+import androidx.constraintlayout.compose.MotionScene
 import com.priyanshumaurya8868.composeutils.ui.theme.ComposeUtilsTheme
+import java.util.*
 
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalMotionApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeUtilsTheme {
-                Box(modifier = Modifier.fillMaxSize(),contentAlignment = Alignment.Center) {
-                    AnimatedCircularProgressbar(percentage = 0.6f, number = 200,strokeWidth = 10.dp, radius = 100.dp, animDuration = 2500)
+                Column {
+                    
+                    var prog  by remember {
+                        mutableStateOf(0f)
+                    }
+                    ProfileHeader(progress = prog)
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Slider(
+                        value = prog,
+                        onValueChange = {
+                            prog = it
+                        },
+                        modifier = Modifier.padding(horizontal = 32.dp)
+                    )
                 }
 
             }
@@ -39,56 +55,49 @@ class MainActivity : ComponentActivity() {
 
 
 }
+
+@ExperimentalMotionApi
 @Composable
-fun AnimatedCircularProgressbar(
-    percentage: Float,
-    number: Int,
-    fontSize: TextUnit = 20.sp,
-    radius: Dp = 50.dp,
-    color: Color = Color.Green,
-    strokeWidth: Dp = 8.dp,
-    animDuration: Int = 1500,
-    animDelay: Int = 0
-) {
-
-    var animationPlayed by remember {
-        mutableStateOf(false)
+fun ProfileHeader(progress: Float) {
+    val context = LocalContext.current
+    val motionScene = remember {
+        context.resources.openRawResource(R.raw.motion_screen)
+            .readBytes()
+            .decodeToString()
     }
 
-    val curPercentage =  animateFloatAsState(
-        targetValue = if (animationPlayed) percentage else 0f,
-        animationSpec = tween(
-            durationMillis = animDuration,
-            delayMillis = animDelay
-        )
-    )
-
-    LaunchedEffect(key1 = true) {
-        animationPlayed = true
-    }
-
-    Box(
-        modifier = Modifier.size(radius * 2f),
-        contentAlignment = Alignment.Center
+    MotionLayout(
+        motionScene = MotionScene(content = motionScene),
+        progress = progress,
+        modifier = Modifier.fillMaxWidth()
     ) {
-        // TODO: its mandatory to define its size
-        Canvas(modifier = Modifier.size(radius)) {
-            drawArc(
-                color = color,
-                startAngle = -90f,
-                sweepAngle = 360 * curPercentage.value,
-                useCenter = false,
-                style = Stroke(width = strokeWidth.toPx(), cap = StrokeCap.Round)
-            )
-
-        }
-        Text(
-            text = (curPercentage.value * number).toInt().toString(),
-            fontSize = fontSize,
-            fontWeight = FontWeight.Bold,
-            color = Color.Gray
+        val properties = motionProperties(id = "profile_pic")
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Gray)
+                .layoutId("box")
         )
-    }
+        Image(
+            painter = painterResource(id = R.drawable.my_pic),
+            contentDescription = "Profile pic",
+            modifier = Modifier
+                .layoutId("profile_pic")
+                .clip(CircleShape)
+                .border(
+                    width = 2.dp,
+                    color = properties.value.color("background"),
+                    shape = CircleShape
+                )
+        )
+        Text(
+            text="Priyanshu Maurya",
+            fontSize = 24.sp,
+            modifier =  Modifier.layoutId("username"),
+            color = properties.value.color("background")
+        )
 
+    }
 }
+
 
